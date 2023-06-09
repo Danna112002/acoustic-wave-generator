84a810b72d52236a764016430d1b00488a8457a1
adziezyk <01169018@pw.edu.pl>
2023-06-09 20:28:59 +0200

    Changes to be committed: 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Create the base sounds
-        BaseAbstractSound sound1 = new SineWaveSound(440, 10, 0);
-        //BaseAbstractSound sound2 = new RampWaveSound(220, 6, 0, 0.7);
-        //BaseAbstractSound sound3 = new SquareWaveSound(660, 4, 0, 0.5);
-
-        // Create the composite sound
-        List<BaseAbstractSound> waveforms = new ArrayList<>();
-        waveforms.add(sound1);
-        //waveforms.add(sound2);
-        //waveforms.add(sound3);
-        // Add more base sounds as needed...
-
-        CompositeSound compositeSound = new CompositeSound(waveforms);
-
-        // Play the composite sound
-        SoundPlayer.playSound(compositeSound);
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new SoundGeneratorGUI();
+            }
+        });
     }
+        
 }
